d39b594741e3319843d435e95fc2372d73c2960a
Matthew Braun <43424316+mjbraun95@users.noreply.github.com>
2023-03-28 19:56:38 -0600

    debugging ask4()

--- bot1.py
+++ bot1.py
@@ -2,9 +2,6 @@ import openai
 import discord
 from discord.ext import commands
 
-# Set up OpenAI API credentials
-openai.api_key = 'YOUR_API_KEY'
-
 # Create a new bot instance with a prefix and intents
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='/', intents=intents)
@@ -20,6 +17,24 @@ async def ask(ctx, *, question):
         n=1,
         stop=None,
         temperature=0.5
+    )
+
+    # Send the response back to the channel
+    await ctx.send(response.choices[0].text)
+
+# GPT-4
+@bot.command()
+async def ask4(ctx, *, question):
+    # Call OpenAI's API to generate a response
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": question},
+                    # {"role": "user", "content": "Who won the world series in 2020?"},
+                    # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                    # {"role": "user", "content": "Where was it played?"}
+                ]
     )
 
     # Send the response back to the channel
@@ -47,10 +62,16 @@ async def hello(ctx):
     # Send a message to the channel where the command was received
     await ctx.send('Hello, world!')
 
-# Acquire bot token
-with open('bot_token.txt', 'r') as f:
-    bot_token = f.read().strip()
+if __name__ == '__main__':
+    # Acquire bot token
+    with open('bot_token.txt', 'r') as f:
+        bot_token = f.read().strip()
 
-# Run the bot
-bot.run(bot_token)
+    with open('api_key.txt', 'r') as f:
+        api_key = f.read().strip()
+        # Set up OpenAI API credentials
+        openai.api_key = api_key
+
+    # Run the bot
+    bot.run(bot_token)
 
