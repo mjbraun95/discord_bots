47b8ef2718a27755cfd7b1e6d9ad0106c3a1dfde
Matthew Braun <43424316+mjbraun95@users.noreply.github.com>
2023-03-29 00:42:22 -0600

    Implemented choosse_prompt() successfully

--- bot1.py
+++ bot1.py
@@ -3,11 +3,14 @@ import discord
 from discord.ext import commands
 import requests
 import json
+import aiohttp
 
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
 # Create a new bot instance with a prefix and intents
 intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 # Acquire bot token
@@ -19,7 +22,8 @@ with open('api_key.txt', 'r') as f:
     api_key = f.read().strip()
     openai.api_key = api_key
 
-def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
+
+async def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}",
@@ -34,12 +38,35 @@ def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=
     if max_tokens is not None:
         data["max_tokens"] = max_tokens
 
-    response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+    async with aiohttp.ClientSession() as session:
+        async with session.post(API_ENDPOINT, headers=headers, json=data) as response:
+            if response.status == 200:
+                response_data = await response.json()
+                return response_data["choices"][0]["message"]["content"]
+            else:
+                raise Exception(f"Error {response.status}: {response.text}")
 
-    if response.status_code == 200:
-        return response.json()["choices"][0]["message"]["content"]
-    else:
-        raise Exception(f"Error {response.status_code}: {response.text}")
+# def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"Bearer {api_key}",
+#     }
+
+#     data = {
+#         "model": model,
+#         "messages": messages,
+#         "temperature": temperature,
+#     }
+
+#     if max_tokens is not None:
+#         data["max_tokens"] = max_tokens
+
+#     response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+
+#     if response.status_code == 200:
+#         return response.json()["choices"][0]["message"]["content"]
+#     else:
+#         raise Exception(f"Error {response.status_code}: {response.text}")
 
 @bot.command()
 async def ask(ctx, *, question):
@@ -49,7 +76,7 @@ async def ask(ctx, *, question):
         {"role": "user", "content": question},
     ]
 
-    response_text = generate_chat_completion(messages=messages)
+    response_text = await generate_chat_completion(messages=messages)
 
     # Send the response back to the channel
     await ctx.send(response_text)
@@ -62,7 +89,7 @@ async def ask3(ctx, *, question):
         {"role": "user", "content": question},
     ]
 
-    response_text = generate_chat_completion(messages=messages, model="gpt-3.5-turbo")
+    response_text = await generate_chat_completion(messages=messages, model="gpt-3.5-turbo")
 
     # Send the response back to the channel
     await ctx.send(response_text)
@@ -81,7 +108,7 @@ async def prompt(ctx, *, prompt):
     # Add the user's question to the messages list
     messages.append({"role": "user", "content": question_message.content})
 
-    response_text = generate_chat_completion(messages=messages)
+    response_text = await generate_chat_completion(messages=messages)
 
     # Send the response back to the channel
     await ctx.send(response_text)
@@ -100,10 +127,62 @@ async def prompt3(ctx, *, prompt):
     # Add the user's question to the messages list
     messages.append({"role": "user", "content": question_message.content})
 
-    response_text = generate_chat_completion(messages=messages, model="gpt-3.5-turbo")
+    response_text = await generate_chat_completion(messages=messages, model="gpt-3.5-turbo")
 
     # Send the response back to the channel
     await ctx.send(response_text)
+
+async def send_long_message(ctx, content, max_length=2000):
+    if len(content) <= max_length:
+        await ctx.send(content)
+    else:
+        while len(content) > max_length:
+            await ctx.send(content[:max_length])
+            content = content[max_length:]
+        await ctx.send(content)
+
+# TODO: Add a prompt list command
+@bot.command()
+async def choose_prompt(ctx):
+    prompts = [
+        "You are a helpful assistant.",
+        "You are an expert on movies.",
+        "You are a history buff.",
+        "You are a travel advisor.",
+    ]
+
+    # Send the list of prompts to the channel
+    prompt_list = "\n".join([f"{i+1}. {prompt}" for i, prompt in enumerate(prompts)])
+    await ctx.send(f"Please choose a prompt by entering its number:\n{prompt_list}")
+
+    # Wait for the user to select a prompt
+    def check_prompt_choice(m):
+        return m.author == ctx.author and m.content.isdigit() and 1 <= int(m.content) <= len(prompts)
+
+
+
+    chosen_prompt_msg = await bot.wait_for('message', check=check_prompt_choice)
+    chosen_prompt_index = int(chosen_prompt_msg.content) - 1
+    chosen_prompt = prompts[chosen_prompt_index]
+
+    # Ask the user for their question
+    await ctx.send("Please enter your question:")
+
+    # Wait for the user's question
+    question_message = await bot.wait_for('message', check=lambda m: m.author == ctx.author)
+
+    # Call OpenAI's API to generate a response
+    messages = [
+        {"role": "system", "content": chosen_prompt},
+        {"role": "user", "content": question_message.content},
+    ]
+
+    response_text = await generate_chat_completion(messages=messages)
+
+    # Send the response back to the channel
+    await send_long_message(ctx, response_text)
+
+# Don't forget to include the new command in your main function, if needed.
 
 @bot.command()
 async def hello(ctx):
