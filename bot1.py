aed6b6f628d733263343b39e8055428c7ff2da3a
Matthew Braun <43424316+mjbraun95@users.noreply.github.com>
2023-03-28 21:17:45 -0600

    testing ask4()

--- bot1.py
+++ bot1.py
@@ -1,10 +1,45 @@
 import openai
 import discord
 from discord.ext import commands
+import requests
+import json
+
+API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
 # Create a new bot instance with a prefix and intents
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+# Acquire bot token
+with open('bot_token.txt', 'r') as f:
+    bot_token = f.read().strip()
+
+# Set up OpenAI API credentials
+with open('api_key.txt', 'r') as f:
+    api_key = f.read().strip()
+    openai.api_key = api_key
+
+def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    }
+
+    data = {
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+    }
+
+    if max_tokens is not None:
+        data["max_tokens"] = max_tokens
+
+    response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        raise Exception(f"Error {response.status_code}: {response.text}")
 
 # Define a new command to generate AI responses
 @bot.command()
@@ -26,15 +61,17 @@ async def ask(ctx, *, question):
 @bot.command()
 async def ask4(ctx, *, question):
     # Call OpenAI's API to generate a response
+    messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question},
+                # {"role": "user", "content": "Who won the world series in 2020?"},
+                # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                # {"role": "user", "content": "Where was it played?"}
+            ]
+    # response = generate_chat_completion(messages=messages)
     response = openai.Completion.create(
         model="gpt-3.5-turbo",
-        messages=[
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": question},
-                    # {"role": "user", "content": "Who won the world series in 2020?"},
-                    # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                    # {"role": "user", "content": "Where was it played?"}
-                ]
+        messages=messages
     )
 
     # Send the response back to the channel
@@ -63,15 +100,15 @@ async def hello(ctx):
     await ctx.send('Hello, world!')
 
 if __name__ == '__main__':
-    # Acquire bot token
-    with open('bot_token.txt', 'r') as f:
-        bot_token = f.read().strip()
 
-    with open('api_key.txt', 'r') as f:
-        api_key = f.read().strip()
-        # Set up OpenAI API credentials
-        openai.api_key = api_key
 
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Translate the following English text to French: 'Hello, how are you?'"}
+    ]
+
+    response_text = generate_chat_completion(messages)
     # Run the bot
     bot.run(bot_token)
 
